b226dea372555ac233b4667f6718fd66f6db8010
Noah <nosherway@yahoo.com>
2024-06-03 10:45:32 -0500

    lecture 174 complete

--- starter/lecture.js
+++ starter/lecture.js
@@ -81,3 +81,52 @@ console.log((2.7).toFixed(0)); //* toFixed always returns a string and not a num
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+//! 173 Lecture: The Remainder Operator 
+ 
+//* The remainder operator simply returns the remainder of a division - see below:
+
+console.log(5 % 2); //* result is 1 and not 2.5, since one is the remainder 
+
+console.log(5 / 2); //* <--- 5 = 2 * 2 = 1 (with one being the remainder)
+
+console.log(8%3);
+
+console.log(8 / 3); //* <--- 8 = 2 * 3 + 2 
+
+console.log(6 % 2 ); //* <--- this returns 0, as the result is an integer. we can use this to check if a number is even or not. 
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function(){
+[...document.querySelectorAll('.movements__row')].forEach(function(row,i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if(i % 3 === 0) row.style.backgroundColor = 'yellow';
+});
+}); 
+
+//! 174 Lecture: Numeric Separators
+
+//* Numeric separators are best for working with really large numbers. 
+
+// const diameter = 2874600000000; //* <--- for example, reading this without commas is unordinary, therefore we can use built in separators
+const diameter = 287_460_000_000; 
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415
+console.log(PI);
+
+//* final note - when we attempt to translate strings to numbers using underscore, it does not work properly. 
+
+console.log(Number('230_000')); //* returns as NaN
+
+
